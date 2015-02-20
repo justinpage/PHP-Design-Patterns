d3e9e4b41a77d297acb2571126550d5820ff9494
Justin Page <xjustinpagex@gmail.com>
2015-02-19 17:01:34 -0800

    update: Null object pattern test with Null Test

--- 06_Null_Pattern/NullObjectPatternTest.php
+++ 06_Null_Pattern/NullObjectPatternTest.php
@@ -5,6 +5,20 @@ require_once __DIR__ . '/../02_Factory_Pattern/Keyboard.php';
 
 class NullObjectPatternTest extends PHPUnit_Framework_TestCase
 {
+	function testNullBehavior()
+	{
+		$this->assertEquals(2, null + 2);
+		$this->assertEquals('nothing', null . 'nothing');
+
+		$this->assertTrue(null == 0);
+		$this->assertTrue(null < -1);
+
+		echo (null) ? "inside" : "outside";
+
+		$this->assertFalse(is_object(null));
+	}
+
+
 	function testReceiptCanAddProductsToItTotal()
 	{
 		$receipt = new Receipt();
